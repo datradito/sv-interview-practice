800f245bbd6d865e631179f53a635cd336c8cda6
Daniel Agustin Tradito <datradito@gmail.com>
2024-01-15 12:03:18 -0300

    feat: adding MUI configuration

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import { AppCacheProvider } from "@mui/material-nextjs/v13-pagesRouter";
+import Layout from "@/components/layout";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AppCacheProvider {...props}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppCacheProvider>
+  );
 }
